5b1f2c563ce44e89892e086d9ee596fa99233c89
lenakrase <krase1997@mail.ru>
2015-11-09 22:33:18 +0700

    HomeWork-1

---
+++ HomeWork/Model/Cat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Cat
+    {
+        private string _name;
+        public Cat(int Age1)
+        {
+            Age = Age1;
+        }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (_name == null)
+                    _name = value;
+            }
+        }
+        public int Age { get; private set; }
+        public int Health { get; private set; } = 5;
+        public void Feed()
+        {
+            Health++;
+        }
+        public void Punish()
+        {
+            Health--;
+        }
+
+        public string CurrentColor
+        {
+            set { }
+            get
+            {
+                if (Health < 5)
+                {
+                    return CatColor.SickColor;
+                }
+                else
+                {
+                    return CatColor.HeathyColor;
+                }
+            }
+        }
+
+    }
+
+}
+
---
+++ HomeWork/View/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace View
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int menu = 0;
+            Console.WriteLine("Задать возраст кошки");
+            var cat = new Cat(Age1: Int32.Parse(Console.ReadLine()));
+            var catColor = new CatColor();
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("Имя:" + cat.Name);
+                Console.WriteLine("Возраст:" + cat.Age);
+                Console.WriteLine("Цвет:" + cat.CurrentColor);
+                Console.WriteLine();
+                Console.WriteLine("1.Задать имя");
+                Console.WriteLine("2.Задать цвет");
+                Console.WriteLine("3.Ударить");
+                Console.WriteLine("4.Покормить");
+
+
+                menu = Int32.Parse(Console.ReadLine());
+                switch (menu)
+                {
+                    case 1:
+                        Console.WriteLine("Введите имя:");
+                        cat.Name = Console.ReadLine();
+                        break;
+                    case 2:
+                        Console.WriteLine("Введите здоровый цвет:");
+                        CatColor.HeathyColor = Console.ReadLine();
+                        Console.WriteLine("Введите больной цвет:");
+                        CatColor.SickColor = Console.ReadLine();
+                        break;
+                    case 3:
+                        cat.Punish();
+                        break;
+                    case 4:
+                        cat.Feed();
+                        break;
+
+                    default:
+                        Console.WriteLine("Неверный пункт меню");
+                        Console.ReadKey();
+                        break;
+                }
+            } while (menu != 0);
+        }
+    }
+}
